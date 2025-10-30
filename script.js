876811d7061d207fdc2359149fe14b5d9f477eaa
Ishika Verma <ishikaverma2317@gmail.com>
2025-10-30 09:47:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+// Bootstrap validation
+(function () {
+  "use strict";
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
